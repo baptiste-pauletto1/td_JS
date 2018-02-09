7f7a739bf225b1d63d25f519f8f7c8da7367a0c3
p16005334 <baptiste.pauletto@etu.univ-amu.fr>
2018-02-09 12:22:13 +0100

    edit from the ta ble

--- js/main.js
+++ js/main.js
@@ -32,9 +32,35 @@
     $(document).ready(function () {
         /* on peut travailler sur le DOM */
 
-        $(".case-blanche").css(css_blanc).hover(goLight,goWhite); // Chaining + callback de bogoss
+        let creer_case_noire = function () {
+            return $('<td />').addClass('case-noire').html('N');
+        };
+        let creer_case_blanche = function () {
+            return $('<td />').addClass('case-blanche').html('B');
+        };
 
-        $(".case-noire").css(css_noir).hover(goLight,goBlack);
+        for (let i = 0;i<8;i++) {
+            let tmpColumn = $('<tr />'), fn1, fn2;
+            if(i%2){
+                fn1 = creer_case_blanche;
+                fn2 = creer_case_noire;
+            } else {
+                fn1 = creer_case_noire;
+                fn2 = creer_case_blanche;
+            }
+            for (let j = 0;j<8;j++) {
+                j%2 ? tmpColumn.append(fn1)
+                    : tmpColumn.append(fn2);
+            }
+            $("#damier").append(tmpColumn);
+        }
+
+        let case_blanche = $(".case-blanche");
+        let case_noire = $(".case-noire");
+
+        case_blanche.css(css_blanc).hover(goLight,goWhite); // Chaining + callback de bogoss
+
+        case_noire.css(css_noir).hover(goLight,goBlack);
     })
 
 
