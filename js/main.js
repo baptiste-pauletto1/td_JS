59548c0e8943fe14356e307cb9471261592d1c74
p16005334 <baptiste.pauletto@etu.univ-amu.fr>
2018-02-09 10:44:44 +0100

    processing the first stuffs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-var monObjet = {
-	nom: "Pauletto"
-};
+(function () {
+    "use strict";
+    let css_blanc = {
+        'background-color' :'black',
+        'color' : 'white',
+        'border' : 'solid 1px white',
+        'font-weight' : 'bolder'
+    };
+    let css_noir = {
+        'background-color' :'white',
+        'color' : 'black',
+        'border' : 'solid 1px black',
+        'font-weight' : 'bolder'
+    };
+
+    $(".case-blanche").css(css_blanc);
+
+    $(".case-noire").css(css_noir);
+
+
+}) ();
