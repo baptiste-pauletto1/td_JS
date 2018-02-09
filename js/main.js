e3cfa05d6292c5cde50658815a2789b29c2c4378
p16005334 <baptiste.pauletto@etu.univ-amu.fr>
2018-02-09 11:26:18 +0100

    first table

--- js/main.js
+++ js/main.js
@@ -12,10 +12,31 @@
         'border' : 'solid 1px black',
         'font-weight' : 'bolder'
     };
+    let css_surbrillance = {
+        'background-color' : 'red',
+        'color' : 'white'
+    };
 
-    $(".case-blanche").css(css_blanc);
+    let goLight = function () {
+        $(this).css(css_surbrillance);
+    };
 
-    $(".case-noire").css(css_noir);
+    let goWhite = function () {
+        $(this).css(css_blanc);
+    };
+
+    let goBlack = function () {
+        $(this).css(css_noir);
+    };
+
+    $(document).ready(function () {
+        /* on peut travailler sur le DOM */
+
+        $(".case-blanche").css(css_blanc).hover(goLight,goWhite); // Chaining + callback de bogoss
+
+        $(".case-noire").css(css_noir).hover(goLight,goBlack);
+    })
+
 
 
 }) ();
